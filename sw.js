37b7be5dffb08d6ffd7cf82cfdf233a4366b8f35
braianruaimi <braianruaimi@gmail.com>
2026-05-13 00:40:16 -0300

    Deploy to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pibesdebarrio-static-20260513030934";
+const CACHE_NAME = "pibesdebarrio-static-20260513033914";
 const CORE_ASSETS = ["./", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
